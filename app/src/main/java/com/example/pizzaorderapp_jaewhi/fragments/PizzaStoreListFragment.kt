b508e7dead88feae70c6af4bccb411f1d3f0d116
Tycoon19 <kevin960831@naver.com>
2021-12-29 22:11:16 +0900

    피자 가게 목록 표시용 프래그먼트 기초 구조 추가

---
+++ app/src/main/java/com/example/pizzaorderapp_jaewhi/fragments/PizzaStoreListFragment.kt
@@ -0,0 +1,29 @@
+package com.example.pizzaorderapp_jaewhi.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.pizzaorderapp_jaewhi.R
+
+class PizzaStoreListFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_pizza_store_list, container, false)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        동작 관련 코드
+
+    }
+
+}
