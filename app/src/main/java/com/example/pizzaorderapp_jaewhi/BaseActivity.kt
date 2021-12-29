6079372483466e8177f804404fe1b0bcfad5466b
Tycoon19 <kevin960831@naver.com>
2021-12-29 15:35:38 +0900

    메인화면이 BaseActivity 기반으로 동작하도록 설정.

--- app/src/main/java/com/example/pizzaorderapp_jaewhi/BaseActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_jaewhi/BaseActivity.kt
@@ -8,4 +8,5 @@ abstract class BaseActivity : AppCompatActivity() {
 
     abstract fun setupEvents()
     abstract fun setValues()
+
 }
--- app/src/main/java/com/example/pizzaorderapp_jaewhi/MainActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_jaewhi/MainActivity.kt
@@ -3,9 +3,26 @@ package com.example.pizzaorderapp_jaewhi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+//  무조건 한 번은 실행을 보장받는 공간이다. 그러므로 맨 앞에 두고 밑의 setupEvents와 setValues를 포함시켜준다.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    override fun setupEvents() {
+
+//        버튼 클릭등의 이벤트 관련 코드 모음.
+
+    }
+
+    override fun setValues() {
+
+//        화면에 데이터를 표시하기 위한 코드 모음.
+
+    }
+
+
 }
