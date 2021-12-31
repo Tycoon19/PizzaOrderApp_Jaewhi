ffa7f5b766cd211cd7197144ff846d03e1cc3ae8
Tycoon19 <kevin960831@naver.com>
2021-12-31 17:02:41 +0900

    TedPermission 활용 -> 전화 권한 획득 후 전화걸기

--- app/src/main/java/com/example/pizzaorderapp_jaewhi/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_jaewhi/ViewStoreDetailActivity.kt
@@ -1,9 +1,15 @@
 package com.example.pizzaorderapp_jaewhi
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.pizzaorderapp_jaewhi.datas.Store
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 
 class ViewStoreDetailActivity : BaseActivity() {
@@ -21,6 +27,33 @@ class ViewStoreDetailActivity : BaseActivity() {
 
 }
     override fun setupEvents() {
+
+        callBtn.setOnClickListener {
+
+            val permissionListener = object : PermissionListener{
+                override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:010-${mStoreData.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL,myUri)
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(mContext,"권한이 없어서 전화 연결에 실패했습니다.",Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
+            TedPermission.create()
+                .setPermissionListener(permissionListener)
+                .setDeniedMessage("전화 연결 권한이 필요합니다. [설정]에서 진행해주세요.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+        }
 
     }
 
